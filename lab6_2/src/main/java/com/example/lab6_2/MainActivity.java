e030fe011029d57d45fc41c5da01bfa178aaffe7
bolovfar <bolovfar@icloud.com>
2022-11-14 23:48:16 +0300

    final lb6

--- lab6_2/src/main/java/com/example/lab6_2/MainActivity.java
+++ lab6_2/src/main/java/com/example/lab6_2/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.lab6_2;
 
-import android.app.SearchManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnReg;
     TextView tvOut;
     EditText login;
-    EditText pswrd;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +22,31 @@ public class MainActivity extends AppCompatActivity {
         btnReg = (Button) findViewById(R.id.button2);
         tvOut = (TextView) findViewById(R.id.tvOut);
         login = (EditText) findViewById(R.id.login);
-        pswrd = (EditText) findViewById(R.id.pswrd);
+        password = (EditText) findViewById(R.id.password);
 
         View.OnClickListener oclBtnSignIn = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                //if(login.getText() = "admin" || pswrd.getText() = "admin")
-               // {
-                //    tvOut.setText("Автогризавция успешно пройдена");
-               // }
+                if(login.getText().toString().equals("admin") &&  password.getText().toString().equals("admin"))
+                {
+                   tvOut.setText("Автогризавция успешно пройдена");
+                }
+                else
+                {
+                    tvOut.setText("Ошибка");
+                }
             }
         };
+        View.OnClickListener oclBtnReg = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+               tvOut.setText("Данная функция в разработке");
+            }
+        };
+
+        btnSignIn.setOnClickListener(oclBtnSignIn);
+        btnReg.setOnClickListener(oclBtnReg);
     }
 }
